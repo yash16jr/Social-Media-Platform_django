7edcf7fc6219168ab54ee996b8314ff6493d6df1
yash16jr <yash16jr@gmail.com>
2023-10-25 12:06:44 +0530

    profile_page

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('logout', views.logout, name = 'logout'),
     path('settings', views.settings, name = 'settings'),
     path('upload', views.upload, name = 'upload'),
-    path('like-post', views.like_post, name='like-post'),
+    path('like-post', views.like_post, name = 'like-post'),
+    path('profile/<str:pk>', views.profile, name = 'profile'),
 
 ]
  
